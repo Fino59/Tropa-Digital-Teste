e8bc38c41d9496465f56e286a1cd875879c3f25b
Danilo Fontes <slipdan@hotmail.com>
2021-10-04 16:22:24 -0300

    Create Button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+
+const Button = styled.button`
+    background-color: #2EAFB2;
+    width: 150px;
+    height: 40px;
+    border-radius: 5px;
+    border: none;
+    color: #fff;
+    font-family: 'Roboto', sans-serif;
+    font-size: 14px;
+    font-weight: 700;
+    transition: ease-in-out 200ms;
+    &:hover{
+        opacity: 0.6;
+    }
+    cursor: pointer;
+`;
+
+export default Button;
