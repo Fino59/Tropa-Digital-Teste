904489d7435321ff7e37512efa0760a340cc1869
Danilo Fontes <slipdan@hotmail.com>
2021-10-05 02:16:30 -0300

    Create routes to home and login pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './index.css';
-import App from './App';
+import App from './pages/Login/App';
+import Home from './pages/Home/index';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+
+    <Switch>
+
+    <Route path="/" component={App} exact />
+
+    <Route path="/home" component={Home} />    
+
+    </Switch>    
+
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
