8765b5c8be8a61b4595203b45797b06523077c3e
Danilo Fontes <slipdan@hotmail.com>
2021-10-05 12:07:24 -0300

    Update Card with Sections for images and text

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,215 @@
 import styled from "styled-components";
 import paisage from '../../assets/images/image-ref.png';
+import lastSeen from '../../assets/images/Frame.png'
+
+
+const UserName = styled.div`
+    display: flex;
+    margin: 40px 0 20px 45px;
+    .hello {
+        color: #a5a5a5;
+        font-size: 25px;
+        font-family: 'Advent Pro', sans-serif;
+        margin: 10px 5px;
+    }
+    .user {
+        color: #959595;
+        font-size: 25px;
+        font-family: 'Advent Pro', sans-serif;
+        font-weight: 700;
+        margin: 10px 5px;
+    }
+`;
+
+const EndSection = styled.div`
+    width: 91%;
+    margin: 0 0 30px 45px;
+    hr {
+        border: solid 1px #d9d9d9;
+    }
+`;
+
+const Cards = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin-left: 40px;
+`;
 
 const DivCard = styled.div`
     width: 500px;
     height: 230px;
     background: #fff;
+    display: flex;
+    flex-direction: column;
+    margin: 10px;
+    border-radius: 5px;
+    transition: ease-in-out 250ms;
+    img {
+        width: 464px;
+        height: 140px;
+        margin: 15px 15px 15px 20px;        
+    }
+    .title-card {
+        font-family: 'Advent Pro', sans-serif;
+        font-size: 14px;
+        font-weight: 700;
+        margin: 0 19px 10px;
+    }
+    .subtitle-card {
+        font-family: 'Advent Pro', sans-serif;
+        font-size: 12px;
+        font-weight: 400;
+        margin: 0 19px;
+    }&:hover {
+        opacity: 0.8;
+        cursor: pointer;
+    }
+
+    .images {
+        position: relative;
+    }
+
+    .visual {
+        width: 100px;
+        height: 30px;
+        background-color: rgba(0, 0, 0, 0.5);
+        border-radius: 5px;
+        position: absolute;
+        z-index: 12;
+        top: 25px;
+        right: 30px;  
+
+        img {
+            width: 25px;
+            height: 20px;
+            position: absolute;
+            top: -10px;
+            right: 50px;
+        }
+        p {
+            font-family: 'Advent Pro', sans-serif;
+            font-size: 11px;
+            color: #fff;
+            position: absolute;
+            top: -3px;
+            right: 15px;
+        }
+    }
+    
 `;
 
 function Card() {
-    return (        
-        <DivCard>
-            <img src={paisage} alt="Paisagem Mar com mulher" />
-        </DivCard>
+    return (
+        <>  
+            <UserName>
+                <p className="hello">Olá</p>
+                <p className="user">Usuário</p>
+            </UserName>
+            <EndSection>
+                <hr></hr>
+            </EndSection>
+            <Cards>                      
+                <DivCard>
+                    <div className="images">
+                        <img src={paisage} alt="Paisagem do Mar com mulher" />
+                        <div className="visual">
+                            <img src={lastSeen} alt="Indica ultima visualização" />
+                            <p>2min atrás</p>
+                        </div>
+                    </div>
+                    <p className="title-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium...</p>
+                    <p className="subtitle-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium animi!</p>
+                </DivCard>
+                <DivCard>
+                    <div className="images">
+                        <img src={paisage} alt="Paisagem do Mar com mulher" />
+                        <div className="visual">
+                            <img src={lastSeen} alt="Indica ultima visualização" />
+                            <p>2min atrás</p>
+                        </div>
+                    </div>
+                    <p className="title-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium...</p>
+                    <p className="subtitle-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium animi!</p>
+                </DivCard>
+                <DivCard>
+                    <div className="images">
+                        <img src={paisage} alt="Paisagem do Mar com mulher" />
+                        <div className="visual">
+                            <img src={lastSeen} alt="Indica ultima visualização" />
+                            <p>2min atrás</p>
+                        </div>
+                    </div>
+                    <p className="title-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium...</p>
+                    <p className="subtitle-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium animi!</p>
+                </DivCard>
+                <DivCard>
+                    <div className="images">
+                        <img src={paisage} alt="Paisagem do Mar com mulher" />
+                        <div className="visual">
+                            <img src={lastSeen} alt="Indica ultima visualização" />
+                            <p>2min atrás</p>
+                        </div>
+                    </div>
+                    <p className="title-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium...</p>
+                    <p className="subtitle-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium animi!</p>
+                </DivCard>
+                <DivCard>
+                    <div className="images">
+                        <img src={paisage} alt="Paisagem do Mar com mulher" />
+                        <div className="visual">
+                            <img src={lastSeen} alt="Indica ultima visualização" />
+                            <p>2min atrás</p>
+                        </div>
+                    </div>
+                    <p className="title-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium...</p>
+                    <p className="subtitle-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium animi!</p>
+                </DivCard>
+                <DivCard>
+                    <div className="images">
+                        <img src={paisage} alt="Paisagem do Mar com mulher" />
+                        <div className="visual">
+                            <img src={lastSeen} alt="Indica ultima visualização" />
+                            <p>2min atrás</p>
+                        </div>
+                    </div>
+                    <p className="title-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium...</p>
+                    <p className="subtitle-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium animi!</p>
+                </DivCard>
+                <DivCard>
+                    <div className="images">
+                        <img src={paisage} alt="Paisagem do Mar com mulher" />
+                        <div className="visual">
+                            <img src={lastSeen} alt="Indica ultima visualização" />
+                            <p>2min atrás</p>
+                        </div>
+                    </div>
+                    <p className="title-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium...</p>
+                    <p className="subtitle-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium animi!</p>
+                </DivCard>
+                <DivCard>
+                    <div className="images">
+                        <img src={paisage} alt="Paisagem do Mar com mulher" />
+                        <div className="visual">
+                            <img src={lastSeen} alt="Indica ultima visualização" />
+                            <p>2min atrás</p>
+                        </div>
+                    </div>
+                    <p className="title-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium...</p>
+                    <p className="subtitle-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium animi!</p>
+                </DivCard>
+                <DivCard>
+                    <div className="images">
+                        <img src={paisage} alt="Paisagem do Mar com mulher" />
+                        <div className="visual">
+                            <img src={lastSeen} alt="Indica ultima visualização" />
+                            <p>2min atrás</p>
+                        </div>
+                    </div>
+                    <p className="title-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium...</p>
+                    <p className="subtitle-card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusantium animi!</p>
+                </DivCard>
+            </Cards>
+        </>
     );
 }
 
